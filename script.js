3490e7edb1a6321f2fd0071446cb929252e7ebd7
RJGATON007 <robertgaton2017@gmail.com>
2021-12-15 15:08:51 +0800

    ggwp

---
+++ script.js
@@ -0,0 +1,65 @@
+//Credits kan Cleven G. Gavarra na naglagay ng javacript para sa portfolio website na ito
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+//Credits kan Abel hona na nag contribute din
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        $('html').css("scrollBehavior", "smooth");
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    var typed = new Typed(".typing", {
+        strings: ["Freelancer", "Photographer", "Rider", "Web dev aspirant"],
+        typeSpeed: 150,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Freelancer", "Photographer", "Rider", "Web dev aspirant"],
+        typeSpeed: 150,
+        backSpeed: 60,
+        loop: true
+    });
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
